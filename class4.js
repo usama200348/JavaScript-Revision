fd5b0aff7ae647cee0fb229706eabf05b3c7cd75
Usama Sohail <135988912+usama200348@users.noreply.github.com>
2026-09-05 00:58:22 +0500

    Variable And Datatypes

---
+++ class4.js
@@ -0,0 +1,11 @@
+var name = "Usama Sohail";
+let age = 23;
+const education = "Iqra University" ;
+
+
+name = "Jawwad Hussain";
+age = 25;
+// let age = 24; Error On This Because Let will not redeclear again the variable of let will only get reassign only
+// education = "Indus University"; Error on this b/c const will  not redeclear or reassign again
+
+console.table([name,age,education]);
