1e691a7fa3f50c0cbb5a9b5ab6636097fb67caaa
Usama Sohail <135988912+usama200348@users.noreply.github.com>
2026-09-05 01:22:21 +0500

    Datatype work done

---
+++ class5.js
@@ -0,0 +1,31 @@
+"use strict" // Writing Code Accroding To New Version Of Javascript
+
+
+//alert("Hello World"); //we cannot use in  nodejs
+
+
+// Readablity that should be priority
+console.log(3+3);
+ console.log("Usama");
+
+
+
+ let name = "Usama Sohail";
+ let age = 23; //int
+ let isLoggedIn = false //Boolean
+
+/*
+            **Data Types In Java Script**
+
+Int = number => 2 to power 53
+bigInt
+String = ""
+Boolean true/false
+null = standalone which is the representation of the empty value
+undefined => value is not assignmed/defined to the variable 
+symbol = To Find the uniqueness  like uniquness in component like react
+object 
+*/
+
+// To Find The Type Of Variable
+console.log(typeof(undefined));
