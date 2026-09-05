525e23ef2c0e9d72ef68e97955e77f8ef516f664
Usama Sohail <135988912+usama200348@users.noreply.github.com>
2026-09-05 18:11:51 +0500

    Function And Object New Added

--- class9.js
+++ class9.js
@@ -9,7 +9,40 @@
 
 // Javascript in dynamic language which means same variable in javascript can hold different datatype 
 
-const score ;
+const score = 100;
+const scoreValue = 10.52;
+const isLoggedIn = false;
+const outTemp  = null;
+const userEmail = undefined;
+
+const id = Symbol('123');
+const anotherId = Symbol('123'); // In symbol return value will not be same
+console.log(id);
+console.log(anotherId);
+console.log(id === anotherId);
 
 
+const bigNumber = 212458107n
+console.log("Big Int Data Type " +  typeof(bigNumber));
 
+// Array 
+const arr = ["Usama", 123,true,523.4];
+// Object
+const myObj = {
+    name : "Usama",
+    age : 23,
+    weight : 88
+};
+
+function myfunc(num1 , num2){
+
+   let num3 = num1+num2  
+   console.log(num3);
+    
+}
+
+myfunc(2,3);
+
+console.log(typeof(myfunc));
+
+// All Premitive Function Are Of Object Type
